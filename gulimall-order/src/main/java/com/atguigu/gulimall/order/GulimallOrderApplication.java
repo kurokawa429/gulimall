bab63fb37d68db015e8c5b5181b93e2b6ffa0da6
kurokawa <zjx4299@163.com>
2023-03-23 22:44:41 +0800

    整合nacos

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -3,9 +3,11 @@ package com.atguigu.gulimall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @MapperScan("com.atguigu.gulimall.order.dao")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallOrderApplication {
 
